b420709280b12461dd6e34dfc6f1387ee1f4ddf0
Alex Kaikatsishvili <alexander.kaykatsishvili@gmail.com>
2018-09-18 13:19:54 +0300

    Version 1.0.1

--- js/main.js
+++ js/main.js
@@ -17,6 +17,21 @@
         }
     });
 
+    // $("#play").on("click", function() {
+    //     $(".video").toggleClass("show");
+    //     $(".close-bg").show();
+    // });
+
+    // $(".get-price").on("click", function() {
+    //     $(".main__content_form").toggleClass("show");
+    //     $(".close-bg-btn").show();
+    // });
+
+    // $(".conf-pol").on("click", function() {
+    //     $(".confidence-policy").toggleClass("show");
+    //     $(".close-bg-policy").show();
+    // });
+
     $("#play").on("click", function() {
         $(".video").toggleClass("show");
         $(".close-bg").show();
@@ -24,18 +39,40 @@
 
     $(".get-price").on("click", function() {
         $(".main__content_form").toggleClass("show");
-        $(".close-bg-btn").show();
+        $(".close-bg").show();
     });
+
+    $(".conf-pol").on("click", function() {
+        $(".confidence-policy").toggleClass("show");
+        $(".close-bg").show();
+    });
+
+    // $(".close-bg").on("click", function() {
+    //     $(".video").toggleClass("show");
+    //     $(".close-bg").hide();
+    // });
 
     $(".close-bg").on("click", function() {
-        $(".video").toggleClass("show");
-        $(".close-bg").hide();
+        if ($(".video").hasClass("show")) {
+            $(".close-bg").hide();
+        }
+        if ($(".main__content_form").hasClass("show")) {
+            $(".close-bg").hide();
+        }
+        if ($(".confidence-policy").hasClass("show")) {
+            $(".close-bg").hide();
+        }
     });
 
-    $(".close-bg-btn").on("click", function() {
-        $(".main__content_form").toggleClass("show");
-        $(".close-bg-btn").hide();
-    });
+    // $(".close-bg-btn").on("click", function() {
+    //     $(".main__content_form").toggleClass("show");
+    //     $(".close-bg-btn").hide();
+    // });
+
+    //  $(".close-bg-policy").on("click", function() {
+    //     $(".confidence-policy").toggleClass("show");
+    //     $(".close-bg-policy").hide();
+    // });
 
     $(".download").on("click", function() {
         $(".status").delay(3000).hide("slow");
