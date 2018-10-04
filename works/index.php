6380060cd188c2a601902bc942a594a829c6845a
Alex Kaikatsishvili <alexander.kaykatsishvili@gmail.com>
2018-10-04 16:39:47 +0300

    Added photogallery yyeah!

---
+++ works/index.php
@@ -0,0 +1,223 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/nanogallery2/dist/jquery.nanogallery2.js"></script>
+    <link href="https://unpkg.com/nanogallery2/dist/css/nanogallery2.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="shortcut icon" href="../img/favicon-ternoprof.ico" type="image/ico">
+    <!-- <script src="https://unpkg.com/nanogallery2/dist/jquery.nanogallery2.js"></script> -->
+    <title>Ternoprof | Наші роботи</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <header class="header flex">
+            <div class="header__logo">
+                <a href="../" class="flex">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="999.92" height="999.92" viewBox="0 0 2081 2081">
+                            <defs>
+                                <style>
+                                .cls-1 {
+                                    fill: #fff;
+                                    fill-rule: evenodd;
+                                }
+                                </style>
+                            </defs>
+                            <path id="logos" class="cls-1" d="M1060.5,20C1635.15,20,2101,485.848,2101,1060.5S1635.15,2101,1060.5,2101,20,1635.15,20,1060.5,485.848,20,1060.5,20Zm92.27,97.443C1156.61,131.976,987.78,428.609,962,462.63l-140.8-2.271,2.27-2.27Q988.1,287.782,1152.77,117.443ZM416.953,485.34H1659.21V735.146H1180.02V871.4L893.872,807.817V735.146H416.953V485.34ZM532.777,757.856H793.946q-11.355,22.707-22.711,45.42Q1276.488,917.948,1781.85,1032.64l-2.27,2.27q-287.265,289.53-574.58,579.1-1.125-157.815-2.27-315.66,34.065-62.445,68.13-124.91-465.51-105.585-931.122-211.2ZM1180.02,1670.79H893.872V1109.86q143.061,31.785,286.148,63.58v497.35Zm-195.307,22.7,140.807,2.28c-0.76.75-1.52,1.51-2.27,2.27q-153.285,152.13-306.594,304.31Q900.677,1847.94,984.713,1693.49Z" transform="translate(-20 -20)" />
+                        </svg>
+                    </div>
+                    <div>
+                        <p class="title">Ternoprof</p>
+                        <p>Електромонтажна фірма</p>
+                    </div>
+                </a>
+            </div>
+            <nav class="header__nav">
+                <ul class="flex">
+                    <li><a href="../">Електромонтаж</a></li>
+                    <li><a href="../video/">Відеоспостереження</a></li>
+                    <li><a href="./" class="active">Наші роботи</a></li>
+                </ul>
+            </nav>
+            <div class="header__nav_icon order-1">
+                <div class="spinner diagonal part-1"></div>
+                <div class="spinner horizontal"></div>
+                <div class="spinner diagonal part-2"></div>
+            </div>
+            <nav class="header__nav--mobile">
+                <ul class="flex">
+                    <li><a href="../">Електромонтаж</a></li>
+                    <li><a href="../video/">Відеоспостереження</a></li>
+                    <li><a href="./" class="active">Наші роботи</a></li>
+                </ul>
+            </nav>
+            <div class="header__contacts">
+                <p><a href="tel:+380967596569">+380 96 759 6569</a></p>
+                <p><a href="tel:+380664491767">+380 66 449 1767</a></p>
+            </div>
+        </header>
+        <main class="main col">
+            <!-- <div class="contacts">
+                <a href="tel:+380967596569">+380 96 759 6569</a>
+                <a href="tel:+380664491767">+380 66 449 1767</a>
+            </div> -->
+            <div style="width: 100%;" ID="ngy2p" data-nanogallery2='{
+                    "itemsBaseURL": "https://ternoprof.herokuapp.com/img/",
+                    "thumbnailWidth": "200",
+                    "locationHash": "false",
+                    "thumbnailHoverEffect2": "imageScaleIn80|imageSepiaOff|labelAppear75",
+                    "thumbnailAlignment": "center"
+                  }'>
+                <a href="Чистовий етап/20160824_162235.jpg"
+                    data-ngkind="album"
+                    data-ngid="1"
+                    data-ngdesc="Description1">Чистовий етап</a>
+                <a href="Чистовий етап/20160803_172243_вулиця Василя Стуса.jpg"
+                    data-ngalbumid="1">Image 1 / album 1</a>
+
+                <a href="Чистовий етап/20160803_172256_вул. Корольова.jpg"
+                    data-ngalbumid="1">Image 2 / album 1</a>
+
+                <a href="Чистовий етап/20160824_162114_Unnamed Road.jpg"
+                    data-ngalbumid="1">Image 1 / album 1</a>
+
+                <a href="Чистовий етап/20160824_162235.jpg"
+                    data-ngalbumid="1">Image 2 / album 1</a>
+
+                <a href="Чистовий етап/20160824_162249.jpg"
+                    data-ngalbumid="1">Image 1 / album 1</a>
+
+                <a href="Чистовий етап/20160824_214803.jpg"
+                    data-ngalbumid="1">Image 2 / album 1</a>
+
+                <a href="Чистовий етап/20161001_233413.jpg"
+                    data-ngalbumid="1">Image 1 / album 1</a>
+
+                <a href="Чистовий етап/20170506_212804.jpg"
+                    data-ngalbumid="1">Image 2 / album 1</a>
+
+                <a href="Чистовий етап/20170506_213259.jpg"
+                    data-ngalbumid="1">Image 1 / album 1</a>
+
+                <a href="Чистовий етап/20170811_184945.jpg"
+                    data-ngalbumid="1">Image 2 / album 1</a>
+
+                <a href="Чистовий етап/20171106_160348.jpg"
+                    data-ngalbumid="1">Image 1 / album 1</a>
+
+                <a href="Чистовий етап/20180105_190906.jpg"
+                    data-ngalbumid="1">Image 2 / album 1</a>
+                    
+                <a href="Чистовий етап/20180323_171704.jpg"
+                    data-ngalbumid="1">Image 1 / album 1</a>
+
+                <a href="Чистовий етап/DSCN9156.JPG"
+                    data-ngalbumid="1">Image 2 / album 1</a>
+
+
+                <a href="Електрика на підприємствах/20180313_100403.jpg"
+                    data-ngkind="album"
+                    data-ngid="2"
+                    data-ngdesc="Description1">Електрика на підприємствах</a>
+                    <a href="Електрика на підприємствах/20180313_100403.jpg"
+                    data-ngalbumid="2">Image 1 / album 1</a>
+                    <a href="Електрика на підприємствах/20180313_100627.jpg"
+                    data-ngalbumid="2">Image 1 / album 1</a>
+                    <a href="Електрика на підприємствах/20180313_100727.jpg"
+                    data-ngalbumid="2">Image 1 / album 1</a>
+                    <a href="Електрика на підприємствах/20180313_101022.jpg"
+                    data-ngalbumid="2">Image 1 / album 1</a>
+                    <a href="Електрика на підприємствах/видео большой щиток.mp4"
+                    data-ngalbumid="2">Image 1 / album 1</a>
+                    <a href="Електрика на підприємствах/торговий центр 2.jpg"
+                    data-ngalbumid="2">Image 1 / album 1</a>
+                    <a href="Електрика на підприємствах/торговий центр.jpg"
+                    data-ngalbumid="2">Image 1 / album 1</a>
+                    <a href="Електрика на підприємствах/швейный цех.jpg"
+                    data-ngalbumid="2">Image 1 / album 1</a>
+
+                    <a href="Додатково/Копия 20180820_202307.jpg"
+                    data-ngkind="album"
+                    data-ngid="3"
+                    data-ngdesc="Description1">Додатково</a>
+                    <a href="Додатково/вокруг камер наблюдения.mp4"
+                    data-ngalbumid="3">Image 1 / album 1</a>
+                    <a href="Додатково/Копия 20180820_202307.jpg"
+                    data-ngalbumid="3">Image 1 / album 1</a>
+
+                    <a href="Чорновий етап/20150127_131813.jpg"
+                    data-ngkind="album"
+                    data-ngid="4"
+                    data-ngdesc="Description1">Чорновий етап</a>
+                    <a href="Чорновий етап/20150127_131813.jpg"
+                    data-ngalbumid="4">Image 1 / album 1</a>
+                    <a href="Чорновий етап/20150211_094544.jpg"
+                    data-ngalbumid="4">Image 1 / album 1</a>
+                    <a href="Чорновий етап/20150211_101009.jpg"
+                    data-ngalbumid="4">Image 1 / album 1</a>
+                    <a href="Чорновий етап/20150211_101032.jpg"
+                    data-ngalbumid="4">Image 1 / album 1</a>
+                    <a href="Чорновий етап/20150211_101243.jpg"
+                    data-ngalbumid="4">Image 1 / album 1</a>
+                    <a href="Чорновий етап/20150213_154430.jpg"
+                    data-ngalbumid="4">Image 1 / album 1</a>
+                    <a href="Чорновий етап/20150213_154531.jpg"
+                    data-ngalbumid="4">Image 1 / album 1</a>
+                    <a href="Чорновий етап/20150629_093556.jpg"
+                    data-ngalbumid="4">Image 1 / album 1</a>
+                    <a href="Чорновий етап/20150629_094611_66-а Садова вул..jpg"
+                    data-ngalbumid="4">Image 1 / album 1</a>
+                    <a href="Чорновий етап/20150629_094953_вул. Костянтина Ареф_єва.jpg"
+                    data-ngalbumid="4">Image 1 / album 1</a>
+                    <a href="Чорновий етап/20170602_020259.jpg"
+                    data-ngalbumid="4">Image 1 / album 1</a>
+                    <a href="Чорновий етап/20170808_030814.jpg"
+                    data-ngalbumid="4">Image 1 / album 1</a>
+                    <a href="Чорновий етап/20180821_111152.jpg"
+                    data-ngalbumid="4">Image 1 / album 1</a>
+                    <a href="Чорновий етап/20180821_111224.jpg"
+                    data-ngalbumid="4">Image 1 / album 1</a>
+                    <a href="Чорновий етап/DSCN4199.jpg"
+                    data-ngalbumid="4">Image 1 / album 1</a>
+                    <a href="Чорновий етап/DSCN8041.jpg"
+                    data-ngalbumid="4">Image 1 / album 1</a>
+                    <a href="Чорновий етап/DSCN8046.jpg"
+                    data-ngalbumid="4">Image 1 / album 1</a>
+                    <a href="Чорновий етап/DSCN8065.jpg"
+                    data-ngalbumid="4">Image 1 / album 1</a>
+                    <a href="Чорновий етап/DSCN9300.jpg"
+                    data-ngalbumid="4">Image 1 / album 1</a>
+                    <a href="Чорновий етап/IMG_7184.jpg"
+                    data-ngalbumid="4">Image 1 / album 1</a>
+            </div>
+        </main>
+        <footer class="footer flex">
+            <div><a href="#" class="conf-pol">Політика конфіденційності</a></div>
+            <div>
+                <p class="text-center">Україна, м. Тернопіль,
+                    <br>вул. Броварна, 2</p>
+            </div>
+            <div><a href="mailto:ternoprof@gmail.com">ternoprof@gmail.com</a></div>
+        </footer>
+        <!-- <video autoplay muted loop id="myVideo">
+        <source src="assets/background.mp4" type="video/mp4">
+        </video> -->
+        <div class="layout">
+        </div>
+        <div class="layout-second">
+        </div>
+        <script src="../js/main.js"></script>
+        <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> -->
+</body>
+
+</html>
